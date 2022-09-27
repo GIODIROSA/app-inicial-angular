c67da9740568af6af2ab60fafb9f2a7cf1fa89e0
Giovanni Di Rosa <giodirosaartes@gmail.com>
2022-09-27 15:33:58 -0300

    evento e interpolacion

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
-  styleUrls: ['./empleado.component.css']
+  styleUrls: ['./empleado.component.css'],
 })
 export class EmpleadoComponent implements OnInit {
+  nombre = 'Juan';
+  apellido = 'Díaz';
+  edad = 18;
+  //empresa = 'Píldoras informáticas';
 
-  constructor() { }
+  /*   getEdad() {
+    return this.edad;
+  } */
 
-  ngOnInit(): void {
-  }
+  /*   llamaEmpresa(value:String){
+  } */
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
