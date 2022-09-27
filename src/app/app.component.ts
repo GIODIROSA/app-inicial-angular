312d46eceeaf05baf880f45ff0249881bc41d3cc
Giovanni Di Rosa <giodirosaartes@gmail.com>
2022-09-26 23:39:41 -0300

    primera app de angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'miprimeraappangular';
+  saludo= "hola a todos... chiquillos";
+}
